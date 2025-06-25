15ec1d44b1381acaa73dcd8e2adbddccb82b87f6
nyaw4027 <nyaw4027@gmail.com>
2025-06-25 18:54:23 +0000

    updated models

--- frontend/models.py
+++ frontend/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class ContactMessage(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
@@ -12,7 +9,7 @@ class ContactMessage(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.email} ({self.created_at.date()})"
-    
+
 
 class TeamMember(models.Model):
     name = models.CharField(max_length=100)
