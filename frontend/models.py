f53d84e8806a23a9cc9b05320663d33f66d27b89
nyaw4027 <nyaw4027@gmail.com>
2025-06-25 22:02:28 +0000

    updated models and also has make migrations

--- frontend/models.py
+++ frontend/models.py
@@ -15,7 +15,7 @@ class TeamMember(models.Model):
     name = models.CharField(max_length=100)
     role = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
-    image = models.ImageField(upload_to='team/', blank=True, null=True)
+    photo = models.ImageField(upload_to='team/', blank=True, null=True)
     order = models.PositiveIntegerField(default=0)
 
     class Meta:
