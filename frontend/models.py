e62a937727a0bffcfaba85c5c8de07953c36e737
nyaw4027 <nyaw4027@gmail.com>
2025-06-25 18:39:09 +0000

    update models

--- frontend/models.py
+++ frontend/models.py
@@ -18,7 +18,7 @@ class TeamMember(models.Model):
     name = models.CharField(max_length=100)
     role = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
-    photo = models.ImageField(upload_to='team_photos/')
+    image = models.ImageField(upload_to='team/', blank=True, null=True)
     order = models.PositiveIntegerField(default=0)
 
     class Meta:
